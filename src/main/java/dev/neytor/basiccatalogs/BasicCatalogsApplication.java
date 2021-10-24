8a82b41196fdc1ff33e5939a8ca64b20be4122ba
victor-orobio <victor.orobio@gmail.com>
2021-10-24 13:09:10 -0500

    Initial configuration

---
+++ src/main/java/dev/neytor/basiccatalogs/BasicCatalogsApplication.java
@@ -0,0 +1,13 @@
+package dev.neytor.basiccatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicCatalogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicCatalogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/neytor/basiccatalogs/BasicCatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.neytor.basiccatalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicCatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
