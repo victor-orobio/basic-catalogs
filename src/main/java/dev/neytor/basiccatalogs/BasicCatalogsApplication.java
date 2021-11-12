9fb446eae0080e2dd8e2c132715611dc16b01bc3
victor-orobio <victor.orobio@gmail.com>
2021-11-11 22:10:09 -0500

    dev: ForwardedHeaderFilter - Using X-Forwarded headers with Spring Boot      https://tomgregory.com/spring-boot-behind-load-balancer-using-x-forwarded-headers/      used to manage https protocol in HATEOAS links

--- src/main/java/dev/neytor/basiccatalogs/BasicCatalogsApplication.java
+++ src/main/java/dev/neytor/basiccatalogs/BasicCatalogsApplication.java
@@ -2,12 +2,29 @@ package dev.neytor.basiccatalogs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+
+import javax.servlet.DispatcherType;
+import java.util.List;
 
 @SpringBootApplication
 public class BasicCatalogsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicCatalogsApplication.class, args);
+	}
+
+	@Bean
+	public FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		ForwardedHeaderFilter filter = new ForwardedHeaderFilter();
+		FilterRegistrationBean<ForwardedHeaderFilter> registration = new FilterRegistrationBean<>(filter);
+		registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC, DispatcherType.ERROR);
+		registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		//registration.setUrlPatterns(List.of("/absoluteURLWithFilter"));
+		return registration;
 	}
 
 }
